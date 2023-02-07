18abf0248265ea0493ee04c208daf988d374ec9a
Mike Byrd <johnhonda41@gmail.com>
2023-02-07 17:39:21 -0500

    Added basic UI input fields, submit button, and UC based Colors

--- app/src/main/java/com/example/uptowncampus/MainActivity.kt
+++ app/src/main/java/com/example/uptowncampus/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.uptowncampus
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.uptowncampus.ui.theme.UptownCampusTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BuildingName("Android")
                 }
             }
         }
@@ -30,14 +34,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BuildingName(name: String) {
+    var buildingName by remember { mutableStateOf("") }
+    var diningOptions by remember { mutableStateOf("") }
+    var activityName by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    Column {
+        OutlinedTextField(
+            value = buildingName,
+            onValueChange = { buildingName = it },
+            label = { Text(stringResource(R.string.buildingName)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = diningOptions,
+            onValueChange = { diningOptions = it },
+            label = { Text(stringResource(R.string.diningOptions)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = activityName,
+            onValueChange = { activityName = it },
+            label = { Text(stringResource(R.string.activityName)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button (
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                Toast.makeText(context, "$buildingName $diningOptions $activityName", Toast.LENGTH_LONG).show()
+            }
+                )
+        {
+            Text(text = "Submit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     UptownCampusTheme {
-        Greeting("Android")
+        BuildingName("Android")
     }
 }
