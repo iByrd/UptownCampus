66eac6539eeb2c81c0098531c204cb9b3dcd0e57
RileySetser <setserr7@gmail.com>
2023-02-23 12:04:52 -0500

    Created Firestore instance in MainViewModel

--- app/src/main/java/com/example/uptowncampus/MainViewModel.kt
+++ app/src/main/java/com/example/uptowncampus/MainViewModel.kt
@@ -6,11 +6,21 @@ import androidx.lifecycle.viewModelScope
 import com.example.uptowncampus.dto.Building
 import com.example.uptowncampus.service.BuildingService
 import com.example.uptowncampus.service.IBuildingService
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
 class MainViewModel(var buildingService : IBuildingService = BuildingService()) : ViewModel() {
 
     var buildings: MutableLiveData<List<Building>> = MutableLiveData<List<Building>>()
+
+    private lateinit var firestore : FirebaseFirestore
+
+    init {
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
 
     fun fetchBuildings() {
         viewModelScope.launch {
