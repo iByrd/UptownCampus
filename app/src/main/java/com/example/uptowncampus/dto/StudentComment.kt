ca449994d08ccd36c3fdf86638f6799117c132e7
Riley Setser <setserra@mail.uc.edu>
2023-02-23 10:05:06 -0500

    Updated comment data class

--- app/src/main/java/com/example/uptowncampus/dto/StudentComment.kt
+++ app/src/main/java/com/example/uptowncampus/dto/StudentComment.kt
@@ -1,5 +1,5 @@
 package com.example.uptowncampus.dto
 
 data class StudentComment(var commentId : String = "", var buildingId : Int = 0, var commentContent : String = "") {
-
+    // TODO: Add an attribute that takes in a photo.
 }
