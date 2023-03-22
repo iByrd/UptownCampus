3ab1ae82a2be8411fbdf54bfc978aba783a893ca
RileySetser <setserr7@gmail.com>
2023-03-22 19:39:06 -0400

    Added SavedBuildings DTO for use with the Firestore database.

---
+++ app/src/main/java/com/example/uptowncampus/dto/SavedBuildings.kt
@@ -0,0 +1,5 @@
+package com.example.uptowncampus.dto
+
+// This data class is used to store buildings into the Firestore database.
+data class SavedBuildings (var buildingId : String = "", var buildingName : String = "") {
+}
--- app/src/main/java/com/example/uptowncampus/dto/StudentComment.kt
+++ app/src/main/java/com/example/uptowncampus/dto/StudentComment.kt
@@ -2,8 +2,6 @@ package com.example.uptowncampus.dto
 
 data class StudentComment(
     var commentId : String = "",
-    var buildingId : Int = 0,
-    var buildingName : String = "",
     var commentContent : String = "") {
     // TODO: Add an attribute that takes in a photo.
 }
