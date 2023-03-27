c541604725388889c22ceafe85c540923dac7c1c
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-27 16:15:05 -0400

    SavedBuildings DTO Suggestion: Remove redundancy in the return line.

--- app/src/main/java/com/example/uptowncampus/dto/SavedBuildings.kt
+++ app/src/main/java/com/example/uptowncampus/dto/SavedBuildings.kt
@@ -3,5 +3,5 @@ package com.example.uptowncampus.dto
 // This data class is used to store buildings into the Firestore database.
 data class SavedBuildings (var buildingId : String = "", var buildingName : String = "") {
     override fun toString(): String {
-        return "$buildingName"
+        return buildingName
 }}
