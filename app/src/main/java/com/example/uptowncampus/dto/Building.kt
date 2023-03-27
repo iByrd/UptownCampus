51161402d289b95c4e2cffec11181bb43410c09b
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-27 16:14:44 -0400

    Building DTO Suggestion: Remove redundancy in the return line.

--- app/src/main/java/com/example/uptowncampus/dto/Building.kt
+++ app/src/main/java/com/example/uptowncampus/dto/Building.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class Building(@SerializedName("id") var buildingId : Int = 0, var buildingName : String) {
     override fun toString(): String {
-        return "$buildingName"
+        return buildingName
     }
 }
