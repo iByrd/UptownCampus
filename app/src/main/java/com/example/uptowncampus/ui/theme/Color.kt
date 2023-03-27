b01a42859f256a6d652d8e08412015feb0f7e79d
ItzSebas10 <77468585+ItzSebas10@users.noreply.github.com>
2023-03-27 16:15:41 -0400

    Removed unused colors from Color.kt

--- app/src/main/java/com/example/uptowncampus/ui/theme/Color.kt
+++ app/src/main/java/com/example/uptowncampus/ui/theme/Color.kt
@@ -3,11 +3,9 @@ package com.example.uptowncampus.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Black = Color( 0xFF000000)
 val Red = Color(0xFFE83941)
 val LightGray = Color(0xFFD0D9FD)
-val DarkRed = Color(0xFFA61F2B)
 val White = Color(0xFFFFFFFF)
